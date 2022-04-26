19bcfd077b50269a4a3bbb3029fc8ca4b7a9d8e8
Schploink <THEKPOC@Gmail.com>
2022-04-25 23:43:00 -0700

    three sum solution

--- Coding-templates/Hooks.js
+++ Coding-templates/Hooks.js
@@ -1,3 +1,37 @@
 useEffect(() => {
   document.title = `You clicked ${count} times`;
 }, [count]); // Only re-run the effect if count changes
+
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function GithubCommit() {
+  const [page, setPage] = useState(1);
+  const [commitHistory, setCommitHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadMoreCommit = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.github.com/search/commits?q=repo:facebook/react+css&page=${page}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/vnd.github.cloak-preview"
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setCommitHistory(response.items);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, [page]);
+}
+
