1f67496d6e4654de9f26147980f84452b75df043
Schploink <THEKPOC@Gmail.com>
2022-06-14 18:03:30 -0700

    Dynamic programming practice

---
+++ Structy-practice/Dynamic.js
@@ -0,0 +1,11 @@
+const tribonacci = (n, memo = {}) => {
+  if (n in memo) return memo[n]
+  if (n <= 1) return 0
+  if (n === 2) return 1
+  
+  memo[n] = tribonacci(n-1, memo) + tribonacci(n-2, memo) + tribonacci(n-3, memo)
+  return memo[n]
+};
+
+// memoization - runtime O(n)
+// without memoization - runtime O(3^n)
