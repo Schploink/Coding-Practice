d2a04ce0feee5ac9594ed6459792953491e066a9
Schploink <THEKPOC@Gmail.com>
2022-06-14 18:23:41 -0700

    Dynamic programming practice

--- Structy-practice/Dynamic.js
+++ Structy-practice/Dynamic.js
@@ -9,3 +9,22 @@ const tribonacci = (n, memo = {}) => {
 
 // memoization - runtime O(n)
 // without memoization - runtime O(3^n)
+
+const sumPossible = (amount, numbers, memo = {}) => {
+  if (amount in memo) return memo[amount]
+  if (amount === 0) return true
+  if (amount < 0) return false
+  
+  for (let num of numbers){
+    if (sumPossible(amount - num, numbers, memo) === true) {
+      memo[amount] = true
+      return true
+    }  
+  }
+  
+  memo[amount] = false
+  return false
+};
+
+// memoization - runtime O(a * n)
+// memoize at return statement
