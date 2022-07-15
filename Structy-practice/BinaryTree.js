4f0fb222cebc850c7dabe74fadd4956bf695c708
Schploink <THEKPOC@Gmail.com>
2022-07-14 21:53:59 -0700

    more recursive traversal practice

--- Structy-practice/BinaryTree.js
+++ Structy-practice/BinaryTree.js
@@ -31,3 +31,24 @@ const depthFirstRecursive = (root) => {
   const rightValues = depthFirstRecursive(root.right);
   return [ root.val, ...leftValues, ...rightValues ];
 };
+
+//breadth first not recursive because recursion stack based
+
+
+const treeSum = (root) => {
+  // traverse tree - collect all values, add them
+  if (root === null) return 0
+  
+  return root.val + treeSum(root.left) + treeSum(root.right)
+};
+
+const treeIncludes = (root, target) => {
+  // return true if value === target else false
+  if (root === null) return false 
+  
+  if (root.val === target) return true
+  const leftDepth = treeIncludes(root.left, target)
+  const rightDepth = treeIncludes(root.right, target)
+  
+  return leftDepth || rightDepth  
+};
