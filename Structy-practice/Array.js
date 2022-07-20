a637d4b3c7bb1f335c2a59c5820b6c50c7327eb2
Schploink <THEKPOC@Gmail.com>
2022-07-19 18:42:58 -0700

    jump game complete

---
+++ Structy-practice/Array.js
@@ -0,0 +1,16 @@
+var canJump = function(nums) {
+    let highestConfirm = nums.length - 1
+    for (let i = nums.length -1; i > -1; i--){
+        if (nums[i] >= highestConfirm - i){
+            highestConfirm = i
+            console.log(nums[i], highestConfirm, i)
+        }      
+    }
+    if (highestConfirm === 0) { 
+        return true 
+    } else {
+        return false
+    }
+    
+};
+// jumpgame 1
