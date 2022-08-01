07891dd00baeae1841948f86f37e06ce305173bd
Schploink <THEKPOC@Gmail.com>
2022-07-31 22:28:25 -0700

    dsna practice

--- Structy-practice/Array.js
+++ Structy-practice/Array.js
@@ -15,3 +15,19 @@ var canJump = function(nums) {
 };
 // jumpgame 1
 // jumpgame 2
+
+var canJump = function(nums) {
+    let highestConfirm = nums.length - 1
+    for (let i = nums.length -1; i > -1; i--){
+        if (nums[i] >= highestConfirm - i){
+            highestConfirm = i
+            console.log(nums[i], highestConfirm, i)
+        }      
+    }
+    if (highestConfirm === 0) { 
+        return true 
+    } else {
+        return false
+    }
+    
+};
